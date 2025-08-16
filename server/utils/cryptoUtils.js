f19df31f86e2b7efbb2a6098182fee7e2a03b96f
shah541-g <syedabdulalishah.786@gmail.com>
2025-08-16 16:06:05 +0500

    Algorithm hidden

--- server/utils/cryptoUtils.js
+++ server/utils/cryptoUtils.js
@@ -1,7 +1,7 @@
 import crypto from "crypto";
 
 // Encryption settings
-const algorithm = 'aes-256-cbc';
+;
 const ENCRYPTION_KEY = Buffer.from(process.env.ENCRYPTION_KEY, 'hex');
 
 // Validate encryption key length
@@ -15,7 +15,7 @@ export const encryptText = (text) => {
     throw new Error('Text to encrypt must be a non-empty string');
   }
   const iv = crypto.randomBytes(16); // Generate unique IV for each encryption
-  const cipher = crypto.createCipheriv(algorithm, ENCRYPTION_KEY, iv);
+  const cipher = crypto.createCipheriv(process.env.ALGORITHM, ENCRYPTION_KEY, iv);
   let encrypted = cipher.update(text, 'utf8', 'hex');
   encrypted += cipher.final('hex');
   return {
@@ -31,7 +31,7 @@ export const decryptText = (encryptedObj) => {
   }
   try {
     const decipher = crypto.createDecipheriv(
-      algorithm,
+      process.env.ALGORITHM,
       ENCRYPTION_KEY,
       Buffer.from(encryptedObj.iv, 'hex')
     );
